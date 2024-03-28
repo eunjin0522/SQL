3ba28c0f80599c244efe0d0bbc7b0d5da1efbef5
JJOGGAN <withmelona1@gmail.com>
2024-03-28 23:49:21 +0900

    연산자 서브쿼리

--- scott_0327.sql
+++ scott_0327.sql
@@ -93,3 +93,56 @@ where e.sal > sq.svg_sal;
 select e.*,(select dname from dept where deptno =e.deptno) as 이름 
     from emp e
 ;
+
+-----------------------0328
+--natural join
+select *
+from emp 
+--cross join dept;
+natural join dept;
+--두 테이블이 갖는 공통 컬럼에 대해서 Inner Join은 별개의 컬럼으로,Natural Join은 하나의 컬럼으로 나타낸다.
+
+----------------------subquery
+--group by 없이 그룹함수 사용 -> 어디에? select에!
+select count(distinct comm) from emp; --comm의 도메인 갯수 : 5개 null값은 카운트 하지 않는다
+select * from emp where empno =7499;
+
+--연산자
+--any
+select empno,ename ,sal from emp
+--where sal > any(select sal from emp where job='SALESNAM'); 
+where sal = any(select sal from emp where job ='SALESMAN'); 
+--괄호 안 1600,1250,1250,1500이며 이것들 중에 하나보다는(any) 크냐 : 1250보다 크냐? 와 비슷하지않나
+--salseman 중 단 한 명만이라도 그 사람보다 많은 급여를 받니?
+
+--all
+--MANAGER 사원들 중 최고 급여보다 많은 급여를 받는 사원들이 조회된다.
+select empno,ename ,sal from emp
+where sal >all(select sal from emp where job='MGR');
+--MANAGER 사원들과 같은 급여를 받는 사원들이 조회된다.
+select empno,ename ,sal from emp
+where sal=all(select sal from emp where job='MGR');
+
+--관리자로 등록되어 있는 사원들을 조회
+select empno,ename ,sal from emp e
+--where exits (select s.empno from emp s where s.mgr = e.empno); --상호연관 쿼리는 join의 역할과 비슷하다
+--데이터의 존재여부 확인 후 찾아줘
+
+-----------------SYSDATE
+select sysdate from dual where sysdate >to_date('2024-03-28 10:36:24','yyyy-mm-dd hh24:mi:ss');
+--to_date의 값보다 sysdate가 크다면 sysdate를 출력해줘
+select sysdate,a from (select sysdate a from dual) where sysdate >to_date('2024-03-28 10:36:24','yyyy-mm-dd hh24:mi:ss');
+
+
+-----rownum : 페이징을 위해 사용된다
+select ename,sal,rownum
+from emp
+order by sal desc;
+--sal의 크기에 맞춰 rownum으로 하고 싶지만 실행 순서로인해 rownum이 먼저 생성된 후 정렬되기 때문에 다르게 나온다
+
+--해결 : 서브쿼리로 만들어버린다
+select t1.*,rownum r2  --t1테이블의 모든 컬럼과 r2를 출력해줘
+from (select ename,sal,rownum from emp order by sal desc)t1;
+
+
+
