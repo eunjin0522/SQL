1c6829438530cf07a3e5d8f9b116889ef77e6c80
JJOGGAN <withmelona1@gmail.com>
2024-03-31 15:31:45 +0900

    추가 실습문제 풀이

--- scott_practice.sql
+++ scott_practice.sql
@@ -1,5 +1,5 @@
 
-
+---------실습1 -----------------------------------------------------
 --1. EMP테이블에서 COMM 의 값이 NULL이 아닌 정보 조회
 select *
 from emp
@@ -76,4 +76,144 @@ where to_char(hiredate,'yy')=81;
 --15. EMP테이블에서 이름이 'E'로 끝나는 직원 조회
 select * 
 from emp
-where ename like '%E';-----??
+where ename like '%E';
+
+--16. EMP테이블에서 이름의 세 번째 글자가 'R'인 직원의 정보 조회 
+SELECT
+    *
+FROM emp
+--where ename like'__R%' --like 사용
+where substr(ename,3,1)='R' --SUBSTR() 함수 사용 
+;
+
+--17. EMP테이블에서 사번, 사원명, 입사일, 입사일로부터 40년 되는 날짜 조회
+select empno,ename, hiredate,add_months(hiredate,40*12) as "40주년" 
+from emp;
+
+--18. EMP테이블에서 입사일로부터 38년 이상 근무한 직원의 정보 조회
+select *
+from emp
+where months_between(sysdate,hiredate) >= 12*38;
+select*from emp;
+
+--19. 오늘 날짜에서 년도만 추출
+select EXTRACT(year from sysdate) as "년도" from dual;
+
+
+---------실습3 -----------------------------------------------------
+--1.
+select e.* ,s.grade 
+from emp e 
+join salgrade s on( e.sal between losal and hisal )
+order by s.grade asc
+;
+
+--2.
+
+select e.* ,s.grade 
+from emp e 
+join salgrade s on( e.sal between losal and hisal )
+where s.grade <= 4
+order by s.grade desc
+;
+
+--3. DEPTNO가 20,30인 부서 사람들의 등급별 평균연봉
+    --조건 : 1. DEPTNO가 20,30인 부서 사람들의 평균연봉을 계산하도록 한다.
+    --2. 연봉 계산은 SAL*12+COMM
+    --3. 순서는 평균연봉이 내림차순으로 정렬한다.
+
+select s.grade , avg(SAL*12 + nvl(comm,0)) as 연봉
+from emp e
+join salgrade s on( e.sal between losal and hisal )
+where e.deptno in ('20','30')
+group by s.grade
+order by 2 DESC;
+
+--4. 1. DEPTNO가 20,30인 부서 사람들의 평균연봉을 조회
+    --2. 연봉 계산은 SAL*12+COMM
+    --3. 순서는 평균연봉이 내림차순으로 정렬한다.
+    
+select deptno , avg(SAL*12 + nvl(comm,0)) as 연봉
+from emp 
+where deptno in ('20','30')
+group by deptno
+order by 2 DESC ;--평균연봉;
+
+--확인용
+select deptno , SAL*12 + nvl(comm,0) as 연봉
+from emp 
+where deptno in ('20','30')
+order by 1 DESC ;
+
+--5.사원의 MGR의 이름을 아래와 같이 Manager컬럼에 조회- 정렬
+select e.empno,e.ename, e.job,e.mgr, m.ename as manager
+from emp e
+left join emp m on e.mgr = m.empno
+order by 1 asc;
+
+--6.사원의 MGR의 이름을 아래와 같이 Manager컬럼에 조회- 정렬
+select e.empno,e.ename, e.job,e.mgr, (select ename  from emp m where e.mgr = m.empno ) as manager
+from emp e
+order by e.mgr ;
+
+--7.MARTIN의 월급보다 많으면서 ALLEN과 같은 부서이거나 20번부서인 사원 조회
+select * 
+from emp
+where sal >(select sal from emp where ename = 'MARTIN') 
+and deptno in ('20',(select deptno from emp where ename='ALLEN'));
+
+--8.‘RESEARCH’부서의 사원 이름과 매니저 이름을 나타내시오.
+-----(1)
+select e.ename, m.ename as manager
+from emp e
+left join emp m on e.mgr = m.empno
+where e.deptno = (select deptno from dept where dname ='RESEARCH')
+;
+-----(2)
+select e.ename, m.ename as manager
+from emp e
+join dept d on d.deptno = e.deptno --using (deptno) USING 절의 열 부분은 식별자를 가질 수 없음
+join emp m on e.mgr = m.empno
+where e.deptno = '20'
+;
+
+--9.GRADE별로 급여를 가장 작은 사원명을 조회
+
+--9.GRADE별로 급여를 가장 작은 사원명을 조회
+
+select grade, ename 
+from emp e
+join salgrade s on( e.sal between s.losal and s.hisal )
+where (e.sal, s.grade) in(select min(e.sal),s.grade from emp e join salgrade s on e.sal between s.losal and s.hisal group by s.grade)
+;
+--10. GRADE별로 가장 많은 급여, 가장 작은 급여, 평균 급여를 조회
+select grade, min(sal) MIN_SAL, max(sal) MAX_SAL , floor(avg(sal)) AVG_SAL
+from emp e
+join salgrade s on( e.sal between s.losal and s.hisal )
+group by s.grade;
+
+--11. GRADE별로 평균급여에 10프로내외의 급여를 받는 사원명을 조회- 정렬
+select s.grade,e.ename as "평균10프로내외인사원"
+from emp e
+join salgrade s on( e.sal between s.losal and s.hisal )
+where e.sal 
+    between(select avg(sal)*0.9 from emp join salgrade s1 on( sal between s1.losal and s1.hisal )where s.grade=s1.grade) 
+    and (select avg(sal)*1.1 from emp join salgrade s2 on( sal between s2.losal and s2.hisal )where s.grade=s2.grade) 
+;
+
+
+--12.지역 재난 지원금을 사원들에게 추가 지급
+-----(조건) :
+----- 1. NEW YORK지역은 SAL의 2%, DALLAS지역은 SAL의 5%, CHICAGO지역은 SAL의 3%,BOSTON지역은 SAL의 7%
+----- 2. 추가지원금이 많은 사람 순으로 정렬
+
+select empno,ename,sal,
+    case when d.loc =  'NEW YORK' then e.sal*1.02
+        when d.loc =  'DALLAS' then e.sal*1.03
+        when d.loc='BOSTON' then e.sal*1.07
+        else 0
+    end as SAL_SUBSIDY
+from emp e
+join dept d using(deptno)
+order by SAL_SUBSIDY desc
+;
